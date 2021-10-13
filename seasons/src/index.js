fc22aab15892dd5c5fddeae142d61a35713c848d
rgederin <gederin.ruslan@gmail.com>
2021-10-13 20:48:33 +0300

    start work with class base components

---
+++ seasons/src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMsg: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude })
+            },
+            err => {
+                this.setState({ errorMsg: err.message })
+            }
+        );
+    }
+
+    render() {
+
+        if (this.state.errorMsg && !this.state.lat) {
+            return <div>Error: {this.state.errorMsg}</div>
+        }
+
+        if (!this.state.errorMsg && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>loading...</div>
+    }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'))
+
