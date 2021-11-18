da0edc01cfa93139e65d2eeeb1f8928d9df7b856
rgederin <gederin.ruslan@gmail.com>
2021-11-18 09:49:45 +0200

    working on form validation with redux form

--- twitch-streams/client/src/components/streams/StreamCreate.js
+++ twitch-streams/client/src/components/streams/StreamCreate.js
@@ -1,9 +1,66 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Field, formValues, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
+
+const StreamCreate = props => {
+
+    // const renderInput = (formProps) => {
+    //     console.log(formProps);
+
+    //     return (
+    //         <input
+    //             onChange={formProps.input.onChange}
+    //             value={formProps.input.value}
+    //         ></input>
+    //     );
+    // }
+
+    const renderInput = ({ input, label, meta }) => {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} autoComplete="off" />
+                {renderError(meta)}
+            </div>
+        );
+    };
+
+    const renderError = ({ touched, error }) => {
+        if (touched && error) {
+            return (<div className="ui error message">
+                <div className="header">{error}</div>
+            </div>);
+        }
+    };
+    const onSubmit = formValues => {
+        console.log(formValues);
+    };
+
     return (
-        <div>StreamCreate</div>
+        <form onSubmit={props.handleSubmit(onSubmit)} className="ui form error">
+            <Field name="title" component={renderInput} label="Enter title" />
+            <Field name="description" component={renderInput} label="Enter description" />
+            <button className="ui button primary">Submit</button>
+        </form>
     );
 };
 
-export default StreamCreate;
+const validate = formValues => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = 'Title field is empty'
+    };
+
+    if (!formValues.description) {
+        errors.description = 'Description field is empty'
+    };
+
+    return errors;
+};
+
+export default reduxForm({
+    form: 'streamCreate',
+    validate: validate
+})(StreamCreate);
