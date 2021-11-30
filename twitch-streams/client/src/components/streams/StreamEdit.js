8dc0e608ebc7431934c7be246ae9a6e18b0fcabd
rgederin <gederin.ruslan@gmail.com>
2021-11-30 22:42:32 +0200

    edit streams

--- twitch-streams/client/src/components/streams/StreamEdit.js
+++ twitch-streams/client/src/components/streams/StreamEdit.js
@@ -1,20 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
+
 
 const StreamEdit = (props) => {
     const dispatch = useDispatch();
     const streamId = props.match.params.id;
-    const stream = useSelector (state => state.streams[streamId])
+    const stream = useSelector(state => state.streams[streamId])
 
     useEffect(() => {
         dispatch(fetchStream(streamId))
     }, []);
 
-    
+    const onSubmit = formValues => {
+        dispatch(editStream(streamId, formValues))
+    };
+
+
+    if (!stream) {
+        return <div>Loading...</div>
+    };
+
     return (
-        <div>StreamEdit</div>
+        <div>
+            <h3>Edit a Stream</h3>
+            <StreamForm
+                initialValues={{ title: stream.title, description: stream.description }}
+                onSubmit={onSubmit} />
+        </div>
     );
 };
 
